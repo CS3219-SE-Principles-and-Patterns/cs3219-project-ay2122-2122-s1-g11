7d81df2556757166efd536a948e5a20c6f0f243f
Murugesan Karthika <mdkkarthika@gmail.com>
2021-09-28 18:40:00 +0800

    basic login and register

---
+++ backend/userService/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require("body-parser"); 
+const cors = require("cors"); 
+const app = express(); 
+
+var corsOptions = {
+    origin: "http://localhost:4001"
+}; 
+
+app.use(cors(corsOptions)); 
+
+app.use(bodyParser.json()); 
+
+app.use(bodyParser.urlencoded({extended: true})); 
+
+const db = require("../userService/models");
+const Role = db.role;
+
+db.sequelize.sync({force: true}).then(() => {
+    console.log('Drop and Resync Db');
+    initial();
+  });
+
+app.get("/", (req, res) => {
+    res.json({message: "Hello there."}); 
+}); 
+
+function initial() {
+    Role.create({
+      id: 1,
+      name: "user"
+    });
+   
+    Role.create({
+      id: 2,
+      name: "admin"
+    });
+  }
+
+require('../userService/routes/auth.routes')(app);
+require('../userService/routes/user.routes')(app);
+
+const PORT = process.env.PORT || 4000; 
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`); 
+}); 
