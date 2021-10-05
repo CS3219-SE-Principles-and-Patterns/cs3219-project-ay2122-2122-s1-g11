6d0ecd07be138e905cfc381a8cf7265f6fe60139
Jonah Goh <jonahgoh@outlook.com>
2021-10-05 19:35:17 +0800

    Re-add back backend folder

---
+++ backend/collaborationService/server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const socket = require("socket.io");
+const cors = require('cors');
+const app = express();
+
+app.use(express());
+app.use(cors());
+
+const server = app.listen(5000, () => {
+    console.log("Socket.io running on port 5000!");
+});
+
+const io = socket(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
+
+const roomCodeTexts = {};
+const roomChatTexts = {};
+
+const getParticipantsInRoom = (roomNo) => {
+    return io.sockets.adapter.rooms.get(roomNo);
+};
+
+const emitMessageToRoom = (socket, roomNo, editorText) => {
+    socket.to(roomNo).emit('message', { roomNo, editorText });
+};
+
+io.on('connection', (socket) => {
+    socket.on('join', roomNo => {
+        socket.join(roomNo);
+        
+        // if there is cached code text from previous session, and there remains people inside the room,
+        // restore cached text
+        if (roomCodeTexts[roomNo] && getParticipantsInRoom(roomNo).size > 1) {
+            console.log(roomCodeTexts[roomNo]);
+            socket.emit('message', { editorText: roomCodeTexts[roomNo] });
+        } else { // else start from clean slate
+            delete roomCodeTexts[roomNo];
+        }
+
+        // if there is cached chat text from previous session, and there remains people inside the room,
+        // restore cached text
+        if (roomChatTexts[roomNo] && getParticipantsInRoom(roomNo).size > 1) {
+            console.log(roomChatTexts[roomNo]);
+            socket.emit('message', { editorText: roomChatTexts[roomNo] });
+        } else { // else start from clean slate
+            delete roomChatTexts[roomNo];
+        }
+    });
+
+    socket.on('keyup', ({ roomNo, editorText }) => {
+        emitMessageToRoom(socket, roomNo, editorText);
+        roomCodeTexts[roomNo] = editorText;
+    });
+
+    socket.on('room', (room) => {
+        console.log(room);
+    });
+
+    // chat 
+    socket.on('chat_send_message', ({ roomNo, message }) => {
+        console.log(message);
+        socket.to(roomNo).emit('chat_new_message', { message });
+    });
+});
---
+++ backend/questionService/models/CATEGORY.js
@@ -0,0 +1,9 @@
+module.exports = Object.freeze({
+    string: "string",
+    array: "array",
+    sorting: "sorting",
+    linkedList: "linked list",
+    trees: "trees",
+    dp: "dynamic programming",
+    graph: "graph",
+});
