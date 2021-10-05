58978ad33825c0aa56682441ee6a720940360bd9
En Hao <larloller@gmail.com>
2021-10-05 21:09:40 +0800

    Add basic routing for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-import TextEditor from './components/TextEditor/TextEditor';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Room from "./pages/Room";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
-  return (
-    <div className="App">
-      {/* to Jonah: temporary for testing, shift this away if required */}
-      <TextEditor />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Switch>
+                    <Route path="/room">
+                        <Room />
+                    </Route>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/" exact>
+                        <Home />
+                    </Route>
+                    <Route path="/">
+                        <ErrorPage />
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/src/pages/Room.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TextEditor from "../components/TextEditor/TextEditor";
+
+const Room = () => {
+    return (
+        <div>
+            <h2>Room Page</h2>
+            <TextEditor />
+        </div>
+    );
+};
+
+export default Room;
