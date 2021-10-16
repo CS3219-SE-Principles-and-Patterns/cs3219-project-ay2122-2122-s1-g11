2083603a98586af9ddbdd315c99e5e0202dcfe3b
Jonah Goh <jonahgoh@outlook.com>
2021-10-17 01:16:16 +0800

    Add backend matching service

--- backend/matchingService/app.js
+++ backend/matchingService/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var queueRouter = require('./routes/queue');
+var queueRouter = require('./routes/match');
 
 var app = express();
 
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/queue', queueRouter);
+app.use('/match', queueRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ backend/matchingService/routes/match.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+const Redis = require('redis');
+const { v4: uuidv4 } = require('uuid');
+
+const redisClient = Redis.createClient();
+
+const EXPIRY_TIME = 80;
+
+/* GET users listing. */
+router.post('/create', function(req, res) {
+  const userId = req.body.user;
+  const difficulty = req.body.difficulty;
+  const questionType = req.body.questionType;
+  const subKey = difficulty + questionType;
+  const key = difficulty + questionType + userId;
+  redisClient.keys(subKey + '*', (error, value) => {
+    if (value.length >= 1) {
+      const partnerKey = value[0];
+      redisClient.get(partnerKey, (error, value1) => {
+        const lobbyId = uuidv4();
+        redisClient.set('matched' + partnerKey, JSON.stringify({lobbyId, userId}));
+        redisClient.del(partnerKey);
+        return res.json({ matchStatus: "success", matchId: lobbyId, parterId: value1 });
+      })
+    } else {
+      redisClient.setex(key, EXPIRY_TIME, userId);
+      return res.json({ matchStatus: "waiting" });
+    }
+  })
+});
+
+router.post('/', function(req, res) {
+  const userId = req.body.user;
+  const difficulty = req.body.difficulty;
+  const questionType = req.body.questionType;
+  const key = difficulty + questionType + userId;
+  redisClient.get('matched' + key, (error, value) => {
+    if (value) {
+      const obj = JSON.parse(value);
+      redisClient.del('matched' + key);
+      return res.json({ matchStatus: "success", matchId: obj.lobbyId, parterId: obj.userId });
+    } else {
+      return res.json({ matchStatus: "waiting" });
+    }
+  })
+})
+
+
+module.exports = router;
