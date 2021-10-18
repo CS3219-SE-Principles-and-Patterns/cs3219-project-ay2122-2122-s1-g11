41303cbaec53473c9afa47a84dd055a92e5478f1
Samuel Lee <samuel.lwy@live.com.sg>
2021-10-18 21:48:22 +0800

    Edit socketio link

--- frontend/src/components/TextEditor/TextEditor.js
+++ frontend/src/components/TextEditor/TextEditor.js
@@ -7,7 +7,7 @@ import "ace-builds/src-noconflict/theme-monokai";
 
 import ChatRoom from '../Chatbox/Chatbox';
 
-const socket = io.connect('http://a2fc3bf466b15441abc7dae0fddc570a-1603244640.ap-southeast-1.elb.amazonaws.com:5000');
+const socket = io.connect('a2fc3bf466b15441abc7dae0fddc570a-1335998832.ap-southeast-1.elb.amazonaws.com:5000');
 
 const TextEditor = () => {
 
