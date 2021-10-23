5863a03f10dbd4f5c69f1d72390d707710fe83ae
Jonah Goh <jonahgoh@outlook.com>
2021-10-23 14:48:33 +0800

    Center spinner

--- frontend/src/components/QuestionSelection/LoadingScreen.jsx
+++ frontend/src/components/QuestionSelection/LoadingScreen.jsx
@@ -6,18 +6,32 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  spinner: {
+    display: 'flex',
+    margin: 'auto',
+    marginTop: '2em'
+  },
+  dialogText: {
+    display: 'flex',
+    margin: 'auto'
+  }
+});
 
 export default function SimpleDialog(props) {
+  const classes = useStyles(props);
 
   return (
     <Dialog onClose={props.handleClose} aria-labelledby="simple-dialog-title" open={props.open}>
       <DialogTitle>Waiting for your peer</DialogTitle>
       <DialogContent>
-        <DialogContentText id="alert-dialog-description">Difficulty: {props.difficulty}</DialogContentText>
-        <DialogContentText id="alert-dialog-description">Category: {props.category}</DialogContentText>
+        <DialogContentText id="alert-dialog-description" className={classes.dialogText}>Difficulty: {props.difficulty}</DialogContentText>
+        <DialogContentText id="alert-dialog-description" className={classes.dialogText}>Category: {props.category}</DialogContentText>  
+        <CircularProgress className={classes.spinner} />
       </DialogContent>
-      <CircularProgress />
-      <DialogActions>
+      <DialogActions >
         <Button onClick={props.handleClose} color="primary">Close</Button>
       </DialogActions>
     </Dialog>
