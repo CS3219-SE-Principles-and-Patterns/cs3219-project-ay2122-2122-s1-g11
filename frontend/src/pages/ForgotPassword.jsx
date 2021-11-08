c0b954be326f27f4757c4317e1a8b6b14dda5dc3
Jonah Goh <jonahgoh@outlook.com>
2021-11-08 21:52:03 +0800

    First entry

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -1,70 +1,116 @@
-import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
-import { Button, Grid, Typography } from "@material-ui/core";
+import React from "react";
+import { Button, TextField, Grid, Paper, Typography } from "@material-ui/core";
+import { withStyles } from "@material-ui/styles";
 import axios from "axios";
+import { withRouter } from "react-router";
 import { endpoints } from "../api/endpoints";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        "& > *": {
-            margin: theme.spacing(1),
-            width: "40ch",
-        },
+const styles = (theme) => ({
+    loginForm: {
+        justifyContent: "center",
+        minHeight: "90vh",
     },
-    form: {
-        marginTop: "5em",
+    buttonBlock: {
+        width: "100%",
     },
-    button: {
-        marginTop: "1em",
+    loginBackground: {
+        justifyContent: "center",
+        minHeight: "30vh",
+        padding: "50px",
     },
-    message: {
-        marginTop: "0.5em",
-    },
-}));
+});
 
-export default function ForgotPassword() {
-    const classes = useStyles();
+class ForgotPassword extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { email: "", error: false, message: "" };
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-    const [email, setEmail] = useState("");
-    const [error, setError] = useState(false);
-    const [message, setMessage] = useState("");
-
-    const handleSubmit = (event) => {
+    async handleSubmit(event) {
         event.preventDefault();
-        setError(false);
+        this.setState({ error: false });
         axios
-            .patch(`${endpoints.userService}/forgotPassword`, { email })
+            .patch(`${endpoints.userService}/forgotPassword`, { email: this.state.email })
             .then((response) => {
-                setMessage(response.data.message);
+                this.setState({ message: response.data.message });
             })
             .catch((error) => {
-                setError(true);
-                setMessage("Email does not exist. Please try again.");
+                this.setState({ error: true });
+                this.setState({ message: "Email does not exist. Please try again." });
             });
     };
 
-    return (
-        <Grid container direction="row" justify="center" spacing={2}>
-            <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
-                <Grid className={classes.form} item>
-                    <TextField
-                        onChange={(event) => setEmail(event.target.value)}
-                        required
-                        label="Email"
-                        name="email"
-                        type="email"
-                    />
+    render() {
+        const { classes } = this.props;
+        return (
+            <Grid container spacing={0} justify="center" direction="row">
+                <Grid item>
+                    <Grid
+                        container
+                        direction="column"
+                        justify="center"
+                        spacing={2}
+                        className={classes.loginForm}
+                    >
+                        <Paper
+                            variant="elevation"
+                            elevation={2}
+                            className={classes.loginBackground}
+                        >
+                            <Grid item>
+                                <Typography component="h1" variant="h5">
+                                    Forgot Password
+                                </Typography>
+                            </Grid>
+                            <Grid item>
+                                <form onSubmit={this.handleSubmit}>
+                                    <Grid container direction="column" spacing={2}>
+                                        <Grid item>
+                                            <TextField
+                                                type="email"
+                                                placeholder="Email"
+                                                fullWidth
+                                                name="email"
+                                                variant="outlined"
+                                                value={this.state.email}
+                                                onChange={(event) =>
+                                                    this.setState({
+                                                        [event.target.name]: event.target.value,
+                                                    })
+                                                }
+                                                required
+                                                autoFocus
+                                            />
+                                        </Grid>
+                                        {this.state.error && (
+                                            <Grid item>
+                                                <Typography
+                                                    color="error"
+                                                >
+                                                    {this.state.message}
+                                                </Typography>
+                                            </Grid>
+                                        )}
+                                        <Grid item>
+                                            <Button
+                                                variant="contained"
+                                                color="primary"
+                                                type="submit"
+                                                className={classes.buttonBlock}
+                                            >
+                                                Submit
+                                            </Button>
+                                        </Grid>
+                                    </Grid>
+                                </form>
+                            </Grid>
+                        </Paper>
+                    </Grid>
                 </Grid>
-                <Grid className={classes.button} item>
-                    <Button onClick={handleSubmit} variant="contained">
-                        Submit
-                    </Button>
-                </Grid>
-                <Grid className={classes.message} item>
-                    <Typography color={error ? "error" : "textSecondary"}>{message}</Typography>
-                </Grid>
-            </form>
-        </Grid>
-    );
+            </Grid>
+        );
+    }
 }
+export default withStyles(styles)(withRouter(ForgotPassword));
+
